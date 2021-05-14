37026c76bfcfe22d52b8e406cb1828e7933b3971
Wenhao Piao <wenhaopiao520@gmail.com>
2021-05-13 22:27:03 -0700

    Add get request on /yelp endpoint

--- src/App.js
+++ src/App.js
@@ -3,8 +3,26 @@ import './App.css';
 import Header from './components/Header.js';
 import Main from './components/Main.js';
 import Footer from './components/Footer.js';
+import axios from 'axios';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      businesses: []
+    }
+  }
+
+  componentDidMount = () => {
+    axios.get(`${process.env.REACT_APP_BACKEND_URL}/yelp`)
+      .then(response => {
+        this.setState({
+          businesses: response.data
+        });
+      })
+      .catch(error => console.log(error))
+  }
+
   render() {
     return (
       <>
