59f0738b746914b2787bedd06a3875843a63bd5d
Daniel Dills <dills253@gmail.com>
2021-05-15 15:12:11 -0700

    added search functionality

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,29 +1,28 @@
 import React from 'react';
 import { Form, Col, Row, Button, Container } from 'react-bootstrap'
 
-
 class SearchBar extends React.Component {
-    render(){
-        return(
-            <>
-            <Container className="bg-dark">
-                <Form inline>
-                    <Row className="justify-content-center">
-                    <Col>
-                        <Form.Control placeholder="Search City" />
-                    </Col>
-                    <Col>
-                        <Form.Control placeholder="Search State" />
-                    </Col>
-                    <Col>
-                        <Button variant="primary" type="submit">GO</Button>
-                    </Col>
-                    </Row>
-                </Form>
-            </Container>
-        </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <Container className="bg-dark">
+          <Form onSubmit={this.props.handleSubmit}>
+            <Row className="justify-content-center">
+              <Col>
+                <Form.Control name="term" placeholder="Keyword" onChange={e => this.props.handleOnChange(e)} value={this.props.term}/>
+              </Col>
+              <Col>
+                <Form.Control name="location" placeholder="City, State" onChange={e => this.props.handleOnChange(e)} value={this.props.location}/>
+              </Col>
+              <Col>
+                <Button variant="primary" type="submit">GO</Button>
+              </Col>
+            </Row>
+          </Form>
+        </Container>
+      </>
+    )
+  }
 }
 
 export default SearchBar;
