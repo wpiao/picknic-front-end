204a4841d8c2e7a165ff189975fcdff81aee9b62
Marleyman876 <grant_garfield@yahoo.com>
2021-05-15 15:50:18 -0700

    added carousel

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import BusinessColumns from './BusinessColumns.js';
+//import BusinessCarousel from'./BusinessCarousel.js'
 
 class Main extends React.Component {
   render() {
     return (
       <>
         <BusinessColumns businesses={this.props.businesses} />
+        {/* <BusinessCarousel restaurant={this.props.businesses}/>  */}
       </>
     )
   }
