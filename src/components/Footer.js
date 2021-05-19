e1739cdabaddfe888fbec83bba0ae4ae0278113d
Wenhao Piao <wenhaopiao520@gmail.com>
2021-05-18 17:51:28 -0700

    Update footer and fix typo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Footer extends React.Component {
   render() {
     return (
-      <footer className="py-3" style={{ background: "#343a40", color: "white", textAlign: "center", margin: "auto" }}>This is Footer</footer>
+      <footer className="py-3" style={{ background: "#343a40", color: "white", textAlign: "center", margin: "auto" }}>&copy; PICKNIC</footer>
     )
   }
 }
