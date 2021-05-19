cfde22a8ea2af6ab0e74579e7de032183d385244
Wenhao Piao <wenhaopiao520@gmail.com>
2021-05-18 17:50:04 -0700

    Add basic Profile component

---
+++ src/components/Profile.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Profile extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>This is Profile component</h1>
+      </>
+    )
+  }
+}
+
+export default Profile;
