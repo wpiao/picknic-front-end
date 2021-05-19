96b8590108cfc164d6f11d135296c1a6f32af602
Wenhao Piao <wenhaopiao520@gmail.com>
2021-05-19 15:56:02 -0700

    Remove handleClick when Business componentat /profile path

--- src/components/Business.js
+++ src/components/Business.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
-import { withRouter } from 'react-router-dom'
+import { withRouter } from 'react-router-dom';
 
 class Business extends React.Component {
   handleClick = () => {
-    const id = this.props.business.id
-    this.props.handleShowcard(id);
-    this.props.history.push(`/business/${id}`)
+    if (this.props.history.location.pathname !== "/profile") {
+      const id = this.props.business.id;
+      this.props.handleShowcard(id);
+      this.props.history.push(`/business/${id}`);
+    }
   }
 
   render() {
     const business = this.props.business;
     return (
       <Card onClick={this.handleClick}>
-        {/* <Card.Img variant="top" src={business.image_url} /> */}
+        <Card.Img variant="top" src={business.image_url} />
         <Card.Body>
           <Card.Title>{business.name}</Card.Title>
-          {/* <Card.Text>{business.location.display_address.join(' ')}</Card.Text> */}
+          <Card.Text>{business.location.display_address.join(' ')}</Card.Text>
           <Card.Text>{business.price}</Card.Text>
           <Card.Text>{`rating: ${business.rating}`}</Card.Text>
         </Card.Body>
